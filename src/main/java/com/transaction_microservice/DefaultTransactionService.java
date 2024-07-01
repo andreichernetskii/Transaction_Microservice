d35651a4d425e625cdb6e3a17459245e9a127556
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-07-01 21:53:06 +0200

    filling service methods

--- src/main/java/com/transaction_microservice/DefaultTransactionService.java
+++ src/main/java/com/transaction_microservice/DefaultTransactionService.java
@@ -1,16 +1,34 @@
 package com.transaction_microservice;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class DefaultTransactionService implements TransactionService {
+    private final TransactionRepository transactionRepository;
+
     @Override
     public ResponseEntity<?> addTransaction( Transaction transaction ) {
-        return null;
+        // Adjust the amount based on the operation type (expense or income)
+        BigDecimal amount = ( transaction.getTransactionType() == TransactionType.EXPENSE )
+                ? transaction.getAmount().negate()
+                : transaction.getAmount();
+
+        transactionRepository.save( new Transaction(
+                null,
+                amount,
+                transaction.getTransactionType(),
+                transaction.getCategory(),
+                transaction.getDate() ) );
+
+//        return ResponseEntity.ok( new MessageResponse( "Financial transaction successfully added." ) );
+        return ResponseEntity.ok("Ok");
     }
 
     @Override
