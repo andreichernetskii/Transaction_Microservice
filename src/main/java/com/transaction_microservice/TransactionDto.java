8da709645898b50d60a9de8a91804c432d1aa3fe
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-07-23 16:21:15 +0200

    added Long id for better usability in the front->back connection

--- src/main/java/com/transaction_microservice/TransactionDto.java
+++ src/main/java/com/transaction_microservice/TransactionDto.java
@@ -1,12 +1,16 @@
 package com.transaction_microservice;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Value;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class TransactionDto {
+    private Long id;
     private BigDecimal amount;
     private TransactionType transactionType;
     private String category;
