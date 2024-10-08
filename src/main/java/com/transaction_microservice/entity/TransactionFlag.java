38f6ef4dfe3852f45c5c66b9448773116566f020
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-10-08 20:59:20 +0200

    transaction flag

---
+++ src/main/java/com/transaction_microservice/entity/TransactionFlag.java
@@ -0,0 +1,19 @@
+package com.transaction_microservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionFlag {
+    @Id
+    private String userId;
+    private boolean sendExpenses;
+}
